c10782ed95ba6a95a5041568e03f92ba2d45c821
cherorafi <80270966+cherorafi@users.noreply.github.com>
2022-10-07 20:09:20 -0400

    Add files via upload

---
+++ Activity 4/TestConsoleApp/Program.cs
@@ -0,0 +1,38 @@
+﻿class Book<T> : Iintroduce
+{
+  private string name = ""; // field
+    public string Name   // property
+  {
+    get { return name; }
+    set { name = value; }
+  }
+
+  private T[] lines = new T[500];
+  public T this[int x]{
+    get {return lines[x];}
+    set {lines[x] = value;}
+  }
+
+
+  public void sayName(){
+    Console.WriteLine("The book is called", name);
+  }
+}
+
+interface Iintroduce
+{
+    void sayName();
+}
+
+
+
+class Program
+{
+  static void Main(string[] args)
+  {
+    var aBook = new Book<string>();
+    aBook.Name = "Curious George";
+    aBook[0] = "George is a monkey";
+    aBook.sayName();
+  }
+}
